deb939813385c9437763e029c23771a9da671564
Nathan9722 <146355716+Nathan9722@users.noreply.github.com>
2023-11-23 14:02:20 +0100

    adding printf.c and printmodulo in the testing branch

---
+++ printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _printf - Emulate the original.
+ *
+ * @format: Format by specifier.
+ *
+ * Return: count of chars.
+ */
+int _printf(const char *format, ...)
+{
+    
