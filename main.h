033b2ecce15420a38c41502bcb47d40da01755a6
NezimK <alimekzine@hotmail.fr>
2023-11-24 18:39:36 +0100

    fix main h

--- main.h
+++ main.h
@@ -7,15 +7,15 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
+int (*get_functions(char *specifier))(va_list);
 int _putchar(char c);
 int printchar(va_list args);
 int printstring(va_list args);
 int printmodulo(va_list args);
 int printdigit(va_list args);
-int get_functions(char convert, va_list args);
 
 typedef struct specifiers {
-    char specifiers;
+    char *specifier;
     int (*f)(va_list);
 } specifiers;
 
