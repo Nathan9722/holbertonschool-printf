171abff61eda6ca0a201d0871aa3456a23105074
Nathan9722 <146355716+Nathan9722@users.noreply.github.com>
2023-11-24 16:32:15 +0100

    fix printf

--- printf.c
+++ printf.c
@@ -2,54 +2,55 @@
 #include <stdarg.h>
 
 /**
- * _printf - Format and print data.
- * @format: Format string.
- * Return: Number of characters printed.
- */
+ 
+_printf - Format and print data.
+@format: Format string.
+Return: Number of characters printed.
+*/
 int _printf(const char *format, ...)
 {
-    va_list args;
-    const char *p;
-    int num = 0;
+	va_list args;
+	const char *p;
+	int num = 0;
 
-    if (format == NULL)
-        return -1;
+	if (format == NULL)
+		return -1;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (p = format; *p; p++)
-    {
-        if (*p == '%' && *(p + 1) == '%')
-        {
-            _putchar(*p);
-            num++;
-            p++;
-        }
-        else if (*p == '%' && *(p + 1) != '%')
-        {
-            char specifier = *(p + 1);
-            int result = 0;
+	for (p = format; *p; p++)
+	{
+		if (p == '%' && (p + 1) == '%')
+		{
+			_putchar(p);
+			num++;
+			p++;
+		}
+		else if (p == '%' && *(p + 1) != '%')
+		{
+			char specifier = (p + 1);
+			int result = 0;
 
-            if (get_functions(specifier, args))
-            {
-                result = get_functions(specifier, args);
-            }
-            else
-            {
-                _putchar('%');
-                _putchar(specifier);
-                result = 2;
-            }
-            num += result;
-            p++;
-        }
-        else
-        {
-            _putchar(*p);
-            num++;
-        }
-    }
+			if (get_functions(specifier, args))
+			{
+				result = get_functions(specifier, args);
+			}
+			else
+			{
+				_putchar('%');
+				_putchar(specifier);
+				result = 2;
+			}
+			num += result;
+			p++;
+		}
+		else
+		{
+			_putchar(p);
+			num++;
+		}
+	}
 
-    va_end(args);
-    return num;
+	va_end(args);
+	return num;
 }
